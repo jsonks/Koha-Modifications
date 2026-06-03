21b944cfa3810709a0cdc8c7b91ceead6ac0212d
jsonks <jrobb@sekls.org>
2026-06-03 10:04:29 -0500

    Update hotspottracker_SQL.sql
    Added withdrawn status

--- Staff/Hotspot Tracker/hotspottracker_SQL.sql
+++ Staff/Hotspot Tracker/hotspottracker_SQL.sql
@@ -9,15 +9,22 @@ i.biblionumber,
 biblio.title,
 i.barcode,
 i.itemcallnumber,
+i.withdrawn,
+i.withdrawn_on,
+av1.lib as withdrawn_desc,
+av2.lib as lost_desc,
+i.notforloan,
 i.damaged,
 i.damaged_on,
-"###-###-####" AS itemnotes_nonpublic,
+i.itemnotes_nonpublic,
 REGEXP_REPLACE(i.itemcallnumber, '[^a-zA-Z\\s]', '') AS base_call,
 REGEXP_SUBSTR(i.itemcallnumber, '[0-9]+') + 0 AS num
 FROM items i
 LEFT JOIN issues s USING (itemnumber)
 LEFT JOIN borrowers b ON (s.borrowernumber = b.borrowernumber)
 LEFT JOIN biblio ON (i.biblionumber = biblio.biblionumber)
+LEFT JOIN authorised_values av1 ON (av1.authorised_value = i.withdrawn AND av1.category = "WITHDRAWN")
+LEFT JOIN authorised_values av2 ON (av2.authorised_value = i.itemlost AND av2.category = "LOST")
 WHERE i.location LIKE '%hotspot%'
   AND i.homebranch = <<Choose Library|branches>>
 ORDER BY base_call, num
